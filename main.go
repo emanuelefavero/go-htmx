4383f25aca5eff2c8f889bf41b43a7c4b3d608f9
emanuelefavero <emafaux89@gmail.com>
2024-04-29 19:21:38 +0200

    add gomon

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	hello := "Hello" // := infers the type of the variable
+	fmt.Println(hello)
 }
 
 // Run the program with: `go run main.go`
+// Or, install gomon (nodemon for go): `go install github.com/julesguesnon/gomon@latest`
+// Add PATH to your .bashrc or .zshrc: `export PATH=$PATH:$HOME/go/bin`
+// Run the program with: `gomon main.go`
