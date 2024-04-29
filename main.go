81211de72a697c5653d34299341709a19c0f27f4
emanuelefavero <emafaux89@gmail.com>
2024-04-29 20:28:19 +0200

    add route handler

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	hello := "Hello" // := infers the type of the variable
-	fmt.Println(hello)
+	// Define the port
+	PORT := "3000"
+	fmt.Println("Server started on http://localhost:" + PORT)
+
+	// Define a route handler
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello World\n")
+		io.WriteString(w, r.Method)
+		// OUTPUT: Hello World GET
+	})
+
+	// Listen on port 8000
+	log.Fatal(http.ListenAndServe(":3000", nil))
+	
+
+	// TIP: log.Fatal is a wrapper around log.Print() followed by a call to os.Exit(1), it logs the error and exits the program if an error occurs
 }
 
 // Run the program with: `go run main.go`
